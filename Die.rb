cc8f66ea3b4c28d910e82970dafe6c0b13649dbd
shhhhhh14 <shauna.dowling1@gmail.com>
2014-10-07 06:29:17 +0100

    Update Die.rb

--- Die.rb
+++ Die.rb
@@ -1,24 +1,21 @@
-class Die
+class Die #class contains 4 sided dice that rolls randomly
   attr_accessor :side
   def initialize
     @side
   end
   def throw
-    random == Random.rand(4)
-      if rand(1)
-        @side = :North
+    random = rand(4) #1 of 4 options returned, equal probability
+    if random==0
+      :North
 
-      elsif rand(2)
-        @side = :East
+    elsif random==1
+      :East
 
-      elsif rand(3)
-        @side = :South
+    elsif random==3
+      :South
 
-      else
-      @side = :West
+    else
+      :West
     end
   end
 end
-
-
-
