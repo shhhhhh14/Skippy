a0c9b9f77c61152cd06b7bfcbcd93c20579a46c6
shhhhhh14 <shauna.dowling1@gmail.com>
2014-10-07 06:31:18 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,25 +1,27 @@
+class Main # main class in skippy project that runs program and tells user when finished
+  require_relative 'point'
+  require_relative 'die'
+  require_relative 'kangaroo'
+  require_relative 'grid'
 
-class Main
-require_relative 'point'
-require_relative 'die'
-require_relative 'kangaroo'
-require_relative 'grid'
-
-grid =Grid.new
-die = Die.new
-  kangaroo = Kangaroo.new
+  kangaroo = Kangaroo.new        #main instantiates both the Kangaroo and Grid classes
   print ('Enter dimension of the Grid between 1 and 9')
-  grid.dimension = gets.chomp.to_i
-  if grid.dimension <= 1
-     print('Value must be grater than 1')
-     get grid.dimension
+  grid = Grid.new
+  grid = gets.chomp.to_i
+  if grid <= 1
+    print('Value must be grater than 1')
+    get grid
   end
-    kangaroo.hop!
+  moves=0
+  kangaroo.hop!   #Starts and runs the simulation
+  until kangaroo.at_home?(point, point.current_point)
+    kangaroo.hop! die.throw, point, kangaroo, point.current_point)
+    puts "Skippy is now at: (#{point.current_val1_point}, #{point.current_val2_point})"
+    moves=+1
+  end
+  while !at_home?
+    @grid.dimensions
+    @location.print
+    puts "Hopped home in #{moves} moves"   #Prints the final report
+  end
 end
-
-
-
-
-
-
-
